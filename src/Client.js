08708de277d8decd7bfa3409725270c1d9b5b1aa
hansputera <47862061+hansputera@users.noreply.github.com>
2020-08-28 18:26:06 +0700

    file per file

---
+++ src/Client.js
@@ -0,0 +1,18 @@
+const { Client } = require('whatsapp-web.js');
+const Enmap = require('enmap');
+const fs = require('fs');
+
+module.exports = class WhatsappBot extends Client {
+  constructor(opt) {
+    super(opt);
+    
+    this.db = new Enmap({ name: 'db' });
+    
+  }
+  
+  build() {
+    require('./module')(this);
+    require('./events')(this);
+    this.initialize();
+  }
+};
