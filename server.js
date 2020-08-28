2f44cb8d3c0cb596c0ace841ee16490c2a44d9aa
hansputera <47862061+hansputera@users.noreply.github.com>
2020-08-28 18:24:44 +0700

    file per file

---
+++ server.js
@@ -0,0 +1,15 @@
+const WA = require('./src/Client');
+const fs = require('fs');
+const SESSION_FILE_PATH = './session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
+const run = new WA({
+  puppeteer: {
+    headless: false,
+    args: ['--no-sandbox', '-disable-setuid-sandbox']
+  },
+  session: sessionCfg
+});
+
