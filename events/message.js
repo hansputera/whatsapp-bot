b7a245cec323686598c00432d7049fff6e6b9204
hansputera <47862061+hansputera@users.noreply.github.com>
2020-08-28 18:28:40 +0700

    Create message.js

---
+++ events/message.js
@@ -0,0 +1,14 @@
+const PREFIX = require('../config.json').PREFIX;
+
+module.exports = async (bot, message) => {
+  if (message.getChat().isGroup) {
+    const msg = message.body.toLowerCase();
+    
+    if (msg.startsWith(PREFIX.toLowerCase())) {
+      require('../src/CommandHandler')(bot, message);
+    }
+  } else {
+    if (!message.getContact().isMyContact) return;
+    
+  }
+}
